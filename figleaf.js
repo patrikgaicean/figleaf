ac8b92bbfdf49014d0ce77e99f614fe7128bbe30
Patrik Gaicean <patrikgaicean07@gmail.com>
2024-02-28 00:30:53 +0200

    refactor file check

--- figleaf.js
+++ figleaf.js
@@ -1,8 +1,19 @@
 import { program } from "./program.js";
 
 const opts = program.opts();
-if (opts.file === undefined) {
-  console.log("not from file");
-} else {
-  console.log("from file:", opts.file);
+const isFile = opts.file !== undefined;
+
+switch (isFile) {
+  case true: {
+    if (program.args.length > 0) {
+      console.error("Can't accept text string if --file option was provided.");
+      break;
+    }
+    console.log("from file:", opts.file);
+    break;
+  }
+  case false: {
+    console.log("not from file");
+    break;
+  }
 }
